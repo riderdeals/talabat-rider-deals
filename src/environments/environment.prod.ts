a68af25493d3c48c81ff4d792daa717126598a3a
riderdeals <devtalabatriderapp@gmail.com>
2025-07-02 15:43:06 +0300

    environmentremoved

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrl: 'https://talabat.maxwellos.com/api' // Production API URL
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: '/api' // Development base URL (use proxy or localhost API URL)
-  };
-  
